572d5c1738cf9a95d04ea3a803859c04a3185437
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 <harshravi324@gmail.com>
2022-01-27 22:01:41 +0530

    Settings file is updated with new apps

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ## Adding self made apps,
+    'products',
+    'profiles',
+    'emails',
 ]
 
 MIDDLEWARE = [
