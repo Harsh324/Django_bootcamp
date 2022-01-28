6d1ebc8fc81ede5639580aa80144e354ed6783d0
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 <harshravi324@gmail.com>
2022-01-28 19:53:21 +0530

    models are registered

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
+from .models import Product
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello world</h1>")
+
+
+# def product_detail_view(request, *args, **kwargs):
+#     obj = Product.objects.get(id = 1)
+#     return HttpResponse(f"Product id {obj.id}")
+
+
+def product_detail_view(request, id):
+    try:
+        obj = Product.objects.get(id = id)
+    except Product.DoesNotExist:
+        raise Http404
+        
+    return HttpResponse(f"Product id {obj.id}")
